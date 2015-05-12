732ebc625a96eb4df31af4f81ce2a639e2804b25
manuelmeister <news.manuelsworld@gmail.com>
2015-05-12 16:44:19 +0200

    First Push

---
+++ Meister/Guestbook/Controller/Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuelmeister
+ * Date: 12.05.15
+ * Time: 14:48
+ */
+
+namespace Meister\Guestbook\Controller;
+
+
+class Controller {
+
+}
---
+++ Meister/Guestbook/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuelmeister
+ * Date: 12.05.15
+ * Time: 15:45
+ */
+
+namespace Meister\Guestbook\Controller;
+
+
+class DefaultController extends Controller {
+
+    public function index()
+    {
+        echo "Test";
+    }
+}
